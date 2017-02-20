539af346b3a113ac5fbfe24950eab37a0279606a
RohitMukherjee24 <rohitjee24@gmail.com>
2017-02-19 23:39:12 -0600

    Adding react with redux books application

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+ export function selectBook(book){
+  //console.log('A Book has been selected!!!', book.title);
+return  {
+   type: 'BOOK_SELECTED',
+   payload: book
+};
+
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+	return [
+		{title: 'javascript: The Good Part', pages: 101},
+		{title: 'Harry potter',  pages: 91},
+		{title: 'The Dark Tower', pages: 11},
+		{title: 'Java concurrency', pages: 81}
+	]
+}
